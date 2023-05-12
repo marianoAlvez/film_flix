ffa787db95d73013a83af8446deea04da5ab13be
Malvez Churrasco <malvez@churrasco.digital>
2023-05-11 21:44:38 -0300

    Add dotenv

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
   const HomeScreen({super.key});
 
- @override
-Widget build(BuildContext context) {
-return const Scaffold(
-body: Placeholder(),
-);
-}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(dotenv.env['OMDB_API_KEY'] ?? 'No exite la api key'),
+      ),
+    );
+  }
 }
